28e4f52d01efc7cd6f1b6aa5850ce357d496c94b
joaobaptista18 <joaobaptistadecarvalhogama@gmail.com>
2023-10-28 20:46:19 +0700

    Tugas4_1818002

---
+++ App.js
@@ -0,0 +1,23 @@
+// import * as React from 'react';
+// import {Home} from './src/screens';
+// export default function App() {
+//   return <Home />;
+// }
+
+import * as React from 'react';
+import {Discover} from './src/screens';
+export default function App() {
+  return <Discover />;
+}
+
+// import * as React from 'react';
+// import {Bookmark} from './src/screens';
+// export default function App() {
+//   return <Bookmark />;
+// }
+
+// import * as React from 'react';
+// import {Profile} from './src/screens';
+// export default function App() {
+//   return <Profile />;
+// }
